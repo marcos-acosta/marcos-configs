07e6b756ab9b2753f5d38e46a626927339868e31
Marcos Acosta <macosta1625@gmail.com>
2023-07-06 13:57:15 -0400

    Add setup for tsserver and pyright

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,8 +1,9 @@
 require("mason").setup()
 require("mason-lspconfig").setup {
-  ensure_installed = { "lua_ls", "tsserver" },
+  ensure_installed = { "lua_ls", "tsserver", "pyright" },
 }
 
+-- Lua
 require("lspconfig").lua_ls.setup {
   settings = {
     Lua = {
@@ -13,3 +14,8 @@ require("lspconfig").lua_ls.setup {
   },
 }
 
+-- JS, TS
+require("lspconfig").tsserver.setup{}
+
+-- Python
+require("lspconfig").pyright.setup{}
