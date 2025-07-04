1c6ca8a3f3709ba72a1f87618c342ebdec5220fc
Marcos Acosta <macosta1625@gmail.com>
2025-07-03 20:08:25 -0400

    Recent updates

--- zsh/scripts/aliases.sh
+++ zsh/scripts/aliases.sh
@@ -2,23 +2,34 @@
 CONFIG_DIR=~/.config
 ALIASES_PATH=~/.config/zsh/scripts/aliases.sh
 
+# GRADLE
+alias gb="./gradlew build"
+alias gr="./gradlew run --stacktrace"
+alias gt="./gradlew test"
+
 # FILESYSTEM
-alias h="cd ~/h"
-alias downloads="cd ~/Downloads"
-alias c="cd ~/h/code"
+alias h="cd ~/marcos"
+alias dl="cd ~/Downloads"
+alias c="cd ~/marcos/code"
 alias ls="ls -A"
+alias notes="code /Users/marcos/marcos/notes/"
+
+# NPM
+alias pub="npm run build && npm publish"
 
 # ZSH SCRIPTS
-alias reload-scripts="source $ALIASES_PATH"
+alias ra="source $ALIASES_PATH"
 
 # RENAMING
 alias python=python3
 
 # EDITING
-alias edit-aliases="nv $ALIASES_PATH"
+alias ea="nv $ALIASES_PATH && ra"
+alias lg="nv /Users/marcos/marcos/notes/log.md"
+alias bd="nv /Users/marcos/marcos/notes/braindump.md"
 
 # APPLICATIONS
-alias nv=/Users/marcos/h/installations/nvim-macos/bin/nvim
+alias nv=/Users/marcos/marcos/installs/nvim-macos/bin/nvim
 
 # CONFIG EDITING
 config ()
@@ -31,9 +42,16 @@ config ()
     cd $CONFIG_DIR/nvim && nv init.lua
   elif [[ $1 = "zsh" ]]; then
     cd $CONFIG_DIR/zsh && nv .zshrc
+  elif [[ $1 = "vim" ]]; then
+    nv ~/.vimrc
   else
     cd $CONFIG_DIR/$1
   fi
 }
 
+# REACT
 
+mc() {
+  touch "$1.tsx"
+  touch "$1.module.css"
+}
